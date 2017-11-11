77cb573c89139fe954a7083680f20773f5edfe1c
张晔10077668 <zhang.ye6@zte.com.cn>
2017-11-11 21:28:04 +0800

    modify

--- python/rfddd/domain/model/repository.py
+++ python/rfddd/domain/model/repository.py
@@ -5,8 +5,14 @@ class Repository:
     def __init__(self):
         self._repo = {}
 
-    def store(self, id, obj):
+    def add(self, id, obj):
         if self._has_key(id):
+            return False
+        self._repo[id] = obj
+        return True
+
+    def save(self, id):
+        if not self._has_key(id):
             return False
         self._repo[id] = obj
         return True
@@ -15,7 +21,7 @@ class Repository:
         if self._has_key(self, id):
             del self._repo[id]
 
-    def find(self, id):
+    def findby(self, id):
         return self._repo.get(id)
 
     def _has_key(self, id):
