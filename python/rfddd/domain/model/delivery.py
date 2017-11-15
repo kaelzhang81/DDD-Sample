f9fc66fb02b50d02c23b5cca678d7d6604254222
kaelzhang81 <zhangy1981@qq.com>
2017-11-15 11:16:53 +0800

    Create delivery.py

---
+++ python/rfddd/domain/model/delivery.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+import value_object
+
+class Delivery(object):
+    def __init__(self, after_days):
+        self._after_days = after_days
+
+ValueObject.register(Delivery)
+
+if __name__ == '__main__':
+    print 'Subclass:', issubclass(Delivery, ValueObject)
+    print 'Instance:', isinstance(Delivery, ValueObject)
