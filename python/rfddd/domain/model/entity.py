65ac82c8d6b19ae11b7b95dde44dee2de4ab2da4
kaelzhang81 <zhangy1981@qq.com>
2017-11-15 11:15:34 +0800

    Update entity.py

--- python/rfddd/domain/model/entity.py
+++ python/rfddd/domain/model/entity.py
@@ -1,9 +1,20 @@
 # coding=utf-8
 
+import abc
 
-class Entity:
+class Entity(object):
+    metaclass=abc.ABCMeta
+
+    _instance_id_generator = count()
+
     def __init__(self, id):
         self._id = id
+        self._instance_id = next(Entity._instance_id_generator)
 
-    def get_id(self):
+    @property
+    def instance_id(self):
+        return self._instance_id
+
+    @property
+    def id(self):
         return self._id
