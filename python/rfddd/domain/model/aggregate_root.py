9e7ff770f994eaa3e13bb533a6a4229f399e5847
kaelzhang81 <zhangy1981@qq.com>
2017-11-15 10:47:49 +0800

    Update aggregate_root.py

--- python/rfddd/domain/model/aggregate_root.py
+++ python/rfddd/domain/model/aggregate_root.py
@@ -1,11 +1,3 @@
 # coding=utf-8
 
-from .entity import Entity
-
-
-class AggregateRoot(Entity):
-    def __init__(self, id):
-        self._id = id
-
-    def get_id(self):
-        return self._id
+from entity import Entity as AggregateRoot
