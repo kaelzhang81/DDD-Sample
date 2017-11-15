6aec6499369426b40bbd1330601a7910edecbe25
kaelzhang81 <zhangy1981@qq.com>
2017-11-15 10:29:18 +0800

    Update repository.py

--- python/rfddd/domain/model/repository.py
+++ python/rfddd/domain/model/repository.py
@@ -1,28 +1,21 @@
 # coding=utf-8
+import abc
 
+class Repository(object):
+    metaclass=abc.ABCMeta
 
-class Repository:
-    def __init__(self):
-        self._repo = {}
+    @abstractmethod
+    def add(self, , id, obj):
+        raise NotImplementedError
 
-    def add(self, id, obj):
-        if self._has_key(id):
-            return False
-        self._repo[id] = obj
-        return True
+    @abstractmethod
+    def save(self, id, obj):
+        raise NotImplementedError
 
-    def save(self, id):
-        if not self._has_key(id):
-            return False
-        self._repo[id] = obj
-        return True
-
+    @abstractmethod
     def delete(self, id):
-        if self._has_key(self, id):
-            del self._repo[id]
+        raise NotImplementedError
 
-    def findby(self, id):
-        return self._repo.get(id)
-
-    def _has_key(self, id):
-        return self._repo.has_key(id)
+    @abstractmethod
+    def find_by_id(self, id):
+        raise NotImplementedError
