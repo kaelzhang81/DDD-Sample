8d8fb56e175c002f1d7797727d9b817ff087d309
kaelzhang81 <zhangy1981@qq.com>
2017-11-16 15:55:36 +0800

    Update delivery.py

--- python/rfddd/domain/model/delivery.py
+++ python/rfddd/domain/model/delivery.py
@@ -1,13 +1,18 @@
 # coding=utf-8
+"""the sample of value object"""
 
 import value_object
 
 class Delivery(object):
+    """delivery class
+    """
+    
     def __init__(self, after_days):
         self._after_days = after_days
 
     @property
     def after_days(self):
+        """the property of after days"""
         return self._after_days
 
 
