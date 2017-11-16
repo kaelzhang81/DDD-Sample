9d64995df5d86dff661992975aa9506906c6d23b
kaelzhang81 <zhangy1981@qq.com>
2017-11-16 16:58:31 +0800

    Update cargo_service.py

--- python/rfddd/domain/service/cargo_service.py
+++ python/rfddd/domain/service/cargo_service.py
@@ -6,17 +6,22 @@ from .delivery import Delivery
 from domain.model import ceargo
 
 class CargoService(object):
+    """cargo service class
+    """
+        
     def __init__(self, cargo_repo, cargo_provider):
         self._cargo_repo = cargo_repo
         self._cargo_provider = cargo_provider
 
     def create(self, id, days):
+        """create cargo"""
         delivery = Delivery(days);
         cargo = Cargo(delivery, id);
         self._cargo_repo.save(cargo);
         self._cargo_provider.confirm(cargo);
 
     def delay(self, id, days):
+        """cargo delay"""
         cargo = self._cargo_repo.find_by_id(id);
         if cargo is not None:
             cargo.Delay(days);
